4d060e68e90032f2f02b0d77b6bd7afe976ef56d
mac_pig <576618121@qq.com>
2018-12-28 07:25:00 +0800

    modify router

--- gcs.go
+++ gcs.go
@@ -5,8 +5,11 @@ import (
 	"github.com/gravida/gcs/models"
 	"github.com/gravida/gcs/pkg/settings"
 	"github.com/gravida/gcs/routers"
+	_ "github.com/mattn/go-sqlite3"
 )
 
+// GOOS=linux GOARCH=amd64 go build -o coolgo_linux *.go
+// GOOS=windows GOARCH=amd64 go build -o coolgo_win *.go
 func main() {
 
 	settings.Setup()
